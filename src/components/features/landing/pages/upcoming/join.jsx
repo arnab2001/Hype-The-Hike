188248fcd4b5af8b3c944c5010009d7912637771
arnab2001 <63441472+arnab2001@users.noreply.github.com>
2022-09-18 03:47:31 +0530

    fixed ui added join us

---
+++ src/components/features/landing/pages/upcoming/join.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const join = () => {
+  return (
+    <div className="join" id="joinus">
+        
+  <button class="learn-more">
+    <span class="circle" aria-hidden="true">
+      <span class="icon arrow"></span>
+    </span>
+    <span class="button-text">Learn More</span>
+  </button>
+
+    </div>
+  )
+}
+
+export default join
